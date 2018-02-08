78df806127389b3f950b9abac611eeb390a1d9b6
Nick Martinez <23nicholasmartinez@gmail.com>
2018-02-07 19:39:51 -0800

    Add python basics

---
+++ python_basics.py
@@ -0,0 +1,45 @@
+##name = input()
+##print(f"hello, {name}!")
+
+##i = 28
+##print(f"i is {i}")
+##f = 2.8
+##print(f"f is {f}")
+##b = True
+##print(f"b is {b}")
+##n = None        ##None type has no value
+##print(f"n is {n}")
+
+##x = 28
+##if x > 0:
+##    print("x is positive")
+##elif x < 0:
+##    print("x is negative")
+##else:
+##    print("x is zero")
+
+##name = "Alice"
+##coordinates = (10.0, 20.0)      ## touple groups values together like x,y coordinates on a graph
+##names = ["Alice", "Bob", "Charlie"]     ## python list
+##print(name[0])
+##print(coordinates[1])
+##print(names[2])
+
+##for i in range(5):
+##    print(i)    ## prints 0 - 4
+
+##names = ["Alice", "Bob"]
+##for name in names:
+##    print(name)
+
+##s = set()   ## collection of items with no duplicates
+##s.add(1)
+##s.add(3)
+##s.add(5)
+##s.add(3)
+##print(s)    {1, 3, 5}
+
+##ages = {"Alice": 22, "Bob": 27}     ## dictionary maps keys to values
+##ages["Charlie"] = 30
+##ages["Alice"] += 1
+##print(ages)
